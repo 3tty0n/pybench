aef6a974008ecef62adc5bc628779bc6ecf44337
Yusuke Izawa <me@yizawa.com>
2023-11-16 16:28:53 +0900

    Parse yaml file and construct command

--- pybench/main.py
+++ pybench/main.py
@@ -3,6 +3,7 @@ import subprocess
 import math
 from pprint import pprint
 
+import yaml
 from cpuinfo import get_cpu_info
 
 
@@ -32,18 +33,15 @@ def _reset_shielding():
         return "failed"
 
 
-
 def _set_no_turbo(with_no_turbo, brand):
-    is_intel = True if brand == 'Intel' else False
-    is_amd = True if brand == 'AMD' else False
     try:
-        if is_intel:
-            value = "1"
+        if brand == "Intel":
+            value = str(int(with_no_turbo))
             with open("/sys/devices/system/cpu/intel_pstate/no_turbo", "w") as nt_file:
                 nt_file.write(value + "\n")
             return with_no_turbo
-        elif is_amd:
-            value = "0"
+        elif brand == "AMD":
+            value = str(int(not with_no_turbo))
             with open("/sys/devices/system/cpu/cpufreq/boost", "w") as nt_file:
                 nt_file.write(value + "\n")
             return with_no_turbo
@@ -53,14 +51,86 @@ def _set_no_turbo(with_no_turbo, brand):
         return "failed"
 
 
+class NotFound(Exception):
+    pass
+
+
+def _parse_conf(path):
+    try:
+        with open(path) as conf_file:
+            conf = yaml.safe_load(conf_file)
+            default_data_file = (
+                conf["default_data_file"]
+                if "default_data_file" in conf.keys()
+                else None
+            )
+            suites = (
+                conf["benchmark_suites"] if "benchmark_suites" in conf.keys() else None
+            )
+            executors = (
+                conf["executors"] if "executors" in conf.keys() else None
+            )
+
+            experiments = (
+                conf["experiments"] if "experiments" in conf.keys() else None
+            )
+    except IOError:
+        raise NotFound("cannot open %s" % path)
+
+    executables = {}
+    for exec_key in executors:
+        executable = executors[exec_key]['path'] + "/" + executors[exec_key]['executable']
+        executables[exec_key] = executable
+
+    for exp_name in experiments:
+        executions = experiments[exp_name]["executions"]
+        for execution in executions:
+            for exec_name in execution:
+                exec_suites_values = execution[exec_name].values()
+                executable = executables[exec_name]
+                for exec_suites in exec_suites_values:
+                    for exec_suite in exec_suites:
+                        suite = suites[exec_suite]
+                        benchmarks = suite['benchmarks']
+                        command = suite['command']
+                        iterations = suite['iterations']
+                        command = command.replace("%(iterations)s", str(iterations))
+
+                        commands = []
+                        if 'variable_values' in suite:
+                            variable_values = suite['variable_values']
+                            for variable_value in variable_values:
+                                newcommand = command.replace("%(variable)s", str(variable_value))
+                                commands.append(executable + " " + newcommand)
+
+                        for benchmark in benchmarks:
+                            benchmark_name = next(iter(benchmark))
+                            extra_args = benchmark[benchmark_name]['extra_args']
+                            variable_values = None
+                            if 'variable_values' in benchmark:
+                                variable_values = benchmark['variable_values']
+
+                            for command in commands:
+                                cmdline = command.replace("%(benchmark)s", benchmark_name)
+                                cmdline = cmdline + str(extra_args)
+                                print(cmdline)
+
+    if not default_data_file:
+        raise NotFound("no default_data_file")
+
+    if not suites:
+        raise NotFound("no benchmark_suites")
+
+    return {"default_data_file": default_data_file, "suites": suites}
+
 
 def _parse_cpu_brand(brand):
-    if 'AMD' in brand:
-        return 'AMD'
-    elif 'Intel' in brand:
-        return 'Intel'
+    if "AMD" in brand:
+        return "AMD"
+    elif "Intel" in brand:
+        return "Intel"
     else:
-        return 'Unknown'
+        return "Unknown"
 
 
 def reset():
@@ -69,20 +139,21 @@ def reset():
 
 def main():
     cpuinfo = get_cpu_info()
-    arch = cpuinfo['arch']
-    brand = _parse_cpu_brand(cpuinfo['brand_raw'])
-    num_core = int(cpuinfo['count'])
+    arch = cpuinfo["arch"]
+    brand = _parse_cpu_brand(cpuinfo["brand_raw"])
+    num_core = int(cpuinfo["count"])
 
     result = activate_shielding(num_core)
     if result == "failed":
         print("setting cpu shielding is failed.")
 
-    # result = _set_no_turbo(with_no_turbo=True, brand=brand)
-    # if result == "failed":
-    #     print("disabling turbo boost is failed.")
+    result = _set_no_turbo(with_no_turbo=True, brand=brand)
+    if result == "failed":
+        print("disabling turbo boost is failed.")
 
     reset()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    _parse_conf("runbench.conf")
