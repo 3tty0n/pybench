61be353f31345fb6f419ff8fe32d3e48fd1ffc98
Yusuke Izawa <me@yizawa.com>
2023-11-16 23:37:20 +0900

    Fix test-executable.py

--- test-executable.py
+++ test-executable.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 
 if __name__ == '__main__':
-    s = """NBody: iterations=1 runtime: 31629us
-NBody: iterations=1 runtime: 29882us
-NBody: iterations=1 runtime: 23657us
-NBody: iterations=1 runtime: 23344us
-NBody: iterations=1 runtime: 23639us
-NBody: iterations=1 runtime: 23581us
-NBody: iterations=1 runtime: 24867us
-NBody: iterations=1 runtime: 23395us
-NBody: iterations=1 runtime: 24267us
-NBody: iterations=1 runtime: 23411us
-"""
-    print(s)
+    txt = "Test1: iterations=1 runtime: 1234us"
+    print(txt)
